ccc623d87d432b91edfee681585f307ed61d3ddf
Minghao Jiang <jiangminghao1001@163.com>
2026-03-25 22:21:45 +0800

    Convert to cask: auto-installs to /Applications, depends on macfuse

---
+++ Casks/mount-manager.rb
@@ -0,0 +1,27 @@
+cask "mount-manager" do
+  version "0.0.1"
+  sha256 "0b73dcfd9d251fdb5c514c0faffc586df9a56630c1aea03e299b0c60627c9333"
+
+  url "https://github.com/mhjiang97/MountManager/releases/download/v#{version}/MountManager.zip"
+  name "MountManager"
+  desc "Menu bar app for managing oxfs SSHFS mounts"
+  homepage "https://github.com/mhjiang97/MountManager"
+
+  depends_on cask: "macfuse"
+
+  app "MountManager.app"
+
+  postflight do
+    system "pipx", "install", "oxfs" unless system("/usr/bin/which", "-s", "oxfs")
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.mountmanager.app.plist",
+  ]
+
+  caveats <<~EOS
+    MountManager requires oxfs (installed via pipx) and macFUSE.
+
+    To start at login, add MountManager via System Settings > General > Login Items.
+  EOS
+end
